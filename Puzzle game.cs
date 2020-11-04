344a7f6872a4ec6c17a8c8f85d4d4d39f6f384d3
Gagandeep Kaur <GK013@atcltd.atc.org.nz>
2020-11-05 11:33:27 +1300

    Add project files.

---
+++ Puzzle game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle_game
+{
+    public partial class btn_puzzle_game : Form
+    {
+        public btn_puzzle_game()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_puzzle_game_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
